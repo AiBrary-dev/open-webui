a9d11634acfeac45b5c19881d23a261d5a7e14c1
fereshtehnm <fereshte.namazi.edu@gmail.com>
2025-02-25 11:22:21 +0330

    Update - auth page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -28,6 +28,12 @@
 
 	let ldapUsername = '';
 
+	const querystringValue = (key) => {
+		const querystring = window.location.search;
+		const urlParams = new URLSearchParams(querystring);
+		return urlParams.get(key);
+	};
+
 	const setSessionUser = async (sessionUser) => {
 		if (sessionUser) {
 			console.log(sessionUser);
@@ -39,7 +45,8 @@
 			$socket.emit('user-join', { auth: { token: sessionUser.token } });
 			await user.set(sessionUser);
 			await config.set(await getBackendConfig());
-			goto('/');
+			const redirectPath = querystringValue('redirect') || '/';
+			goto(redirectPath);
 		}
 	};
 
@@ -160,7 +167,7 @@
 		>
 		<div class="bg-[#5612CC] w-full flex flex-col items-center justify-center min-h-screen">
 			<!-- Big Title in the center -->
-			<h1 class="text-6xl font-bold text-white text-center mb-4">Aibrary Chat</h1>
+			<h1 class="text-6xl font-bold text-white text-center mb-4">AiBrary Chat</h1>
 	
 			<!-- Links at the bottom -->
 			<div class="absolute bottom-10 w-full text-center">
@@ -274,6 +281,17 @@
 									</div>
 								</div>
 							{/if}
+							{#if $WEBUI_NAME == "AiBrary"}
+								<div class="flex flex-col mt-4">
+									<button
+											class="bg-gray-700/5 hover:bg-gray-700/10 dark:bg-gray-100/5 dark:hover:bg-gray-100/10 dark:text-gray-300 dark:hover:text-white transition w-full rounded-full font-medium text-sm py-2.5"
+											type="submit"
+									>
+										signin
+									</button>
+
+								</div>
+							{/if}
 							<div class="mt-5">
 								{#if $config?.features.enable_login_form || $config?.features.enable_ldap}
 									{#if mode === 'ldap'}
@@ -341,21 +359,7 @@
 											window.location.href = `${WEBUI_BASE_URL}/oauth/google/login`;
 										}}
 									>
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="size-6 mr-3">
-											<path
-												fill="#EA4335"
-												d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-											/><path
-												fill="#4285F4"
-												d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
-											/><path
-												fill="#FBBC05"
-												d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
-											/><path
-												fill="#34A853"
-												d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
-											/><path fill="none" d="M0 0h48v48H0z" />
-										</svg>
+										
 										<span>{$i18n.t('continue to AiBrary')}</span>
 									</button>
 								{/if}
